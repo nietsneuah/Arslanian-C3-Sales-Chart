5e6933a657ad790151c8d3a496da9e51a6253816
Doug Hauenstein <doug.hauenstein@gmail.com>
2022-12-27 17:22:43 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,96 @@
+// import c3 library
+import c3 from 'c3';
+
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+// This calls the loadChart script on FM (loadChart returns json as a string)
+window.loadChart = function (json) {
+  console.log('loadChart' , json);
+  const obj = JSON.parse(json);
+  // JSON.parse() will convert the json string to a JSON object that JS can use (looks like {data: [{Array 1}, {Array 2}]}   )
+  const data = obj.data;
+  console.log('obj', obj);
+  console.log('data', data);
+
+  
+
+  const chartType = obj.chartType;
+
+  
+  const chartHeight = obj.chartHeight;
+  const chartWidth = obj.chartWidth;
+  
+  const options = {
+    bindto: '#chart',
+    axis: {
+      x: {type: 'category'},
+      y: {},
+    },
+    size: {
+      height: chartHeight,
+      width: chartWidth,
+    },
+    data: {
+      onclick: function (d){
+        console.log('onclick data', d);
+        // // is called "deconstruction"
+      const {index, id, value, name} = (d);
+      // // console.log("Index", index);
+      const month = months[index];
+      const newObj = {month , name, value};
+      console.log("new object", newObj);
+      // // Call a FM script (scriptName, @param) in this case passing the data at from onclick event as a new JSON object that is converted to a string (.stringify)
+      FileMaker.PerformScript("On Chart Click 1", JSON.stringify (newObj));
+      },
+      labels: true,
+      type: chartType,
+      json: data,
+      keys: {
+        x: 'month',
+        value: ['Apples', 'Peaches','Pears'],
+    },
+  },
+
+
+  }
+  const chart = c3.generate(options);
+
+  window.transformChart = function(type) {
+    chart.transform(type);
+  };
+
+  window.resizeChart = function(height, width) {
+    chart.resize({
+      height: height,
+      width: width
+    })
+  };
+  
+  // Bring in new data to existing chart
+  window.loadData = function (json) {
+    const obj = JSON.parse(json);
+    const data = obj.data;
+    console.log(data);
+    chart.load({
+      json: data,
+        keys: {
+          x: 'month',
+          value: ['Bananas'],
+      },
+    });
+  
+  };
+};
