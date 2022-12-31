0c9ded872bb5f848395a61ccc83d893832b2b70a
Doug Hauenstein <doug.hauenstein@gmail.com>
2022-12-31 13:45:53 -0500

    dynamic dateLabels

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 // import c3 library
 import c3 from 'c3';
 
-const months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
+// const months = [
+//   "Jan",
+//   "Feb",
+//   "Mar",
+//   "Apr",
+//   "May",
+//   "Jun",
+//   "Jul",
+//   "Aug",
+//   "Sep",
+//   "Oct",
+//   "Nov",
+//   "Dec",
+// ];
 
 // This calls the loadChart script on FM (loadChart returns json as a string)
 window.loadChart = function (json) {
@@ -29,6 +29,36 @@ window.loadChart = function (json) {
   const chartType = obj.chartType;
   const chartHeight = obj.chartHeight;
   const chartWidth = obj.chartWidth;
+
+  const len  = obj.data.length;
+  console.log('len of array', len);
+
+  // construct array of dateLabels
+  const dateLables = [];
+  for (let i = 0; i < len; i++) {
+    dateLables.push({
+      key: obj.data[i].dateLabel,
+      sortable: true,
+      resizable: true
+    });
+  }
+  console.log('arr from loop', dateLables);
+
+  // determine the grouping based on chart type
+function grouping (c){
+    let result;
+    if (chartType === 'bar') {
+      result = [
+        ['Rug sales'],
+        ['AR cleaning','moth', 'AR misc', 'AR treatment', 'pads'],
+        ['Steam Clean', 'Scotchgard', 'Deodorize'], 
+        ['Carpet', 'Furniture', 'Tile']];
+    } else {
+      result = '';
+    }
+    return result;
+  };
+  console.log('grouping', grouping(chartType));
   
   const options = {
     bindto: '#chart',
@@ -41,6 +71,8 @@ window.loadChart = function (json) {
       width: chartWidth,
     },
     data: {
+      groups: grouping(chartType),
+      
       // groups: [
       //   ['Rug sales'],
       //   ['AR cleaning','moth', 'AR misc', 'AR treatment', 'pads'],
@@ -53,16 +85,38 @@ window.loadChart = function (json) {
         x: 'dateLabel',
         value: ['AR cleaning','AR treatment', 'AR misc', 'moth', 'pads', 'Rug sales'],
     },
+    // pie: {
+    //   label:{
+    //     format: function (value, ratio, id) {
+    //       return d3.format('$')(value);
+    //     }
+    //   }
+    // },
       onclick: function (d){
         console.log('onclick data', d);
+        console.log('chart type', chartType);
+
+        // test function to check type of chart
+        function typeofChart (c){
+          let result;
+          if (chartType === 'bar') {
+            result = 'Use a dateLabel in the result';
+          } else {
+            result = 'its a ' + chartType + ' chart';
+          }
+          return result;
+        };
+          console.log('conditional',typeofChart(chartType));
+        
         // // is called "deconstruction"
-      const {value, name} = (d);
-      // // console.log("Index", index);
+      const {value, name, index} = (d);
+      console.log("Index", index);
       // const month = months[index];
-      const newObj = {name, value};
+      const dataLabel = dateLables[index].key;
+      const newObj = {name, value, dataLabel};
       console.log("new object", newObj);
       // // Call a FM script (scriptName, @param) in this case passing the data at from onclick event as a new JSON object that is converted to a string (.stringify)
-      FileMaker.PerformScript("On Chart Click 1", JSON.stringify (newObj));
+      FileMaker.PerformScript("On Chart Click", JSON.stringify (newObj));
       },
 
       
@@ -92,7 +146,7 @@ window.loadChart = function (json) {
       json: data1,
         keys: {
           x: 'dateLabel',
-          value: ['Carpet', 'Furniture', 'Scotchgard', 'Deodorize', 'Tile'],
+          value: ['Carpet', 'Furniture', 'Scotchgard', 'Deodorize', 'Tile', 'Steam Clean'],
           axis: {
             x: {type: 'category'},
             y: {},
